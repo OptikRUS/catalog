acd17147a7d8c4abe7a158f164645146c588b2d5
optikrus <optikrus61@gmail.com>
2022-06-08 13:39:52 +0300

    add docker nginx

--- catalog/settings.py
+++ catalog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-mq%u5u%hjc6gyg$!lgephf0-ec-2_zjan@mm0*itbje(j3-t4z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1']
 
 # SITE_ID = 1: ru.catalog.com
 # SITE_ID = 2: en.catalog.com
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': 'catalog',
         'USER': 'optikrus',
         'PASSWORD': 'password123456',
-        'HOST': '127.0.0.1',
-        'PORT': '54325',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
@@ -130,8 +130,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'templates',
 ]
+STATIC_ROOT = BASE_DIR / '..' / 'nginx' / 'static'
 
 
 # Default primary key field type
